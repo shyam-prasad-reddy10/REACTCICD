e13db62bdc208965304fe891c1dbf3da192735b7
shyam-prasad-reddy10 <shyamprasad2004@gmail.com>
2025-07-26 11:34:25 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
+
 import './App.css'
 import Hello from './components/Hello'
 import Counter from './components/Counter'
